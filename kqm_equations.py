57d6ba2c2d288a42fa8207be164e5ddd7620e8db
Paolo Acchiardi <48619429+paoloacchiardi@users.noreply.github.com>
2021-01-29 22:39:52 +0100

    Update kqm_equations.py
    ax^2 + bx + c = (kx + q)^2 + m
    Calculate k,q,m starting from a,b,c.

--- kqm_equations.py
+++ kqm_equations.py
@@ -1,6 +1,7 @@
 import math
 
 def main():
+    change_sign = False
     while True:
         a = float(input("Insert value of a: "))
         b = float(input("Insert value of b: "))
@@ -15,14 +16,16 @@ def main():
         a = -a
         b = -b
         c = -c
+        change_sign = True
+        print("Change sign of a,b,c.")
     k = math.sqrt(a) #k = a^(1/2)
     q = b/(2*k)
     m = c - math.pow(q,2)
     print("k = a^(1/2),\tq = b/(2*a^(1/2)),\tm = c - (b/(2*a^(1/2)))^2")
     print(f"k = {format(k, '.2f')}\tq = {format(q, '.2f')}\tm = {format(m, '.2f')}")
-    if a > 0:
+    if(not change_sign): #if i haven't change mathematical sign
         print("result -> (kx+q)^2 + m")
-    else: # a < 0
+    else:
         print("result -> -[(kx+q)^2 + m]")   
           
 if __name__ == "__main__":
